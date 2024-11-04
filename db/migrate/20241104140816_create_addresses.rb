da57f7a5c929c481c1c0256129e14ee34490a243
Everton Silva <everton.p.silva@outlook.com>
2024-11-04 12:43:39 -0300

    fix: remove reference to report in addresses migration

--- db/migrate/20241104140816_create_addresses.rb
+++ db/migrate/20241104140816_create_addresses.rb
@@ -1,7 +1,6 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-      t.references :report, null: false, foreign_key: true
       t.string :cep
       t.string :street
       t.string :neighbhood
