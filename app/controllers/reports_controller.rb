2e8e3cb0fbd5fd10fb0621f68c20fd9b2a0220f2
Everton Silva <everton.p.silva@outlook.com>
2024-11-04 12:41:57 -0300

    feat: add ReportsController with CRUD actions

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,29 @@
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+    #render json: @reports
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to reports_path, notice: 'Report was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :description, :category_id, :address_id)
+  end
+end
